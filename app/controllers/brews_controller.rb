9bfe498114c50b16bbd0205ec92f1355d16afffb
dermetma <demms@me.com>
2025-06-23 15:09:35 -0700

    Add rest of CRUD and views

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -1,8 +1,12 @@
 class BrewsController < ApplicationController
   before_action :set_current_user
+  before_action :set_brew, only: %i[ show edit update destroy ]
 
   def index
       @brews = @current_user.brewer.brews.all
+  end
+
+  def show
   end
 
   def new
@@ -23,9 +27,34 @@ class BrewsController < ApplicationController
       end
   end
 
+  def update
+    respond_to do |format|
+      if @brew.update(brew_params)
+        format.html { redirect_to @brew, notice: "Brew was successfully updated." }
+        format.json { render :show, status: :ok, location: @brew }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @brew.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @brew.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to brews_path, status: :see_other, notice: "Brew was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
   private
   def set_current_user
       @current_user = Current.user
+  end
+
+  def set_brew
+    @brew = Brew.find(params.expect(:id))
   end
 
   def brew_params
