2a552563bda8db16c61560325fbd2e66351cbe22
dermetma <demms@me.com>
2025-06-23 11:09:51 -0700

    Associate users and brewers

--- app/models/brewer.rb
+++ app/models/brewer.rb
@@ -1,2 +1,3 @@
 class Brewer < ApplicationRecord
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_23_173411) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_23_180449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_23_173411) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_brewers_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -37,5 +39,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_23_173411) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "brewers", "users"
   add_foreign_key "sessions", "users"
 end
